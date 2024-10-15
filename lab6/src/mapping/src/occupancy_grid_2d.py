74bc61503dc9484e7b1b999fe3612a00db95cfcf
riensou <ryancampbell@berkeley.edu>
2024-10-15 12:28:04 -0700

    started Lab 6

--- lab6/src/mapping/src/occupancy_grid_2d.py
+++ lab6/src/mapping/src/occupancy_grid_2d.py
@@ -51,14 +51,14 @@ class OccupancyGrid2d(object):
 
         # Dimensions and bounds.
         # TODO! You'll need to set values for class variables called:
-        # -- self._x_num
-        # -- self._x_min
-        # -- self._x_max
-        # -- self._x_res # The resolution in x. Note: This isn't a ROS parameter. What will you do instead?
-        # -- self._y_num
-        # -- self._y_min
-        # -- self._y_max
-        # -- self._y_res # The resolution in y. Note: This isn't a ROS parameter. What will you do instead?
+        self._x_num = rospy.get_param("~x/num")
+        self._x_min = rospy.get_param("~x/min")
+        self._x_max = rospy.get_param("~x/max")
+        self._x_res = (self._x_max - self._x_min) / self._x_num # The resolution in x. Note: This isn't a ROS parameter. What will you do instead?
+        self._y_num = rospy.get_param("~y/num")
+        self._y_min = rospy.get_param("~y/min")
+        self._y_max = rospy.get_param("~y/max")
+        self._y_res = (self._y_max - self._y_min) / self._y_num # The resolution in y. Note: This isn't a ROS parameter. What will you do instead?
 
         # Update parameters.
         if not rospy.has_param("~update/occupied"):
@@ -80,16 +80,19 @@ class OccupancyGrid2d(object):
             return False
         self._free_threshold = self.ProbabilityToLogOdds(
             rospy.get_param("~update/free_threshold"))
+        
+        #print(rospy.get_param_names())
 
         # Topics.
         # TODO! You'll need to set values for class variables called:
-        # -- self._sensor_topic
-        # -- self._vis_topic
+        self._sensor_topic = rospy.get_param("~topics/sensor")
+        self._vis_topic = rospy.get_param("~topics/vis")
 
         # Frames.
         # TODO! You'll need to set values for class variables called:
-        # -- self._sensor_frame
-        # -- self._fixed_frame
+    
+        self._sensor_frame =rospy.get_param("~frames/sensor")
+        self._fixed_frame = rospy.get_param("~frames/fixed")
 
         return True
 
