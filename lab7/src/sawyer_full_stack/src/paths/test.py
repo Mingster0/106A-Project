9b7c4da1293c40d1e65c98362e70d629da09f7b2
Aayushi Chaudhry <54677434+aayushichaudhry@users.noreply.github.com>
2024-12-20 16:36:37 -0800

    Delete lab7 directory

--- lab7/src/sawyer_full_stack/src/paths/test.py
+++
@@ -1,7 +0,0 @@
-import rospy
-import moveit_commander
-
-rospy.init_node('test_moveit_connection')
-moveit_commander.roscpp_initialize(sys.argv)
-group = moveit_commander.MoveGroupCommander("right_arm")
-print("MoveIt! Connection Successful")
